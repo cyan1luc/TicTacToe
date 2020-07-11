e806555d3460b9f6527780667dfd60bcdc3c6464
Collin <cyan1@luc.edu>
2020-07-11 16:20:13 -0500

    class to be executed.

---
+++ TicTacToe/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+	public static void main(String [] args){
+//		Player p1 = new Player('X');
+//		Player p2 = new Player('O');
+//		Player current = p2;
+//		Player winner = null;
+//		Board board = new Board(3,3);
+//		TicTacToe test = new TicTacToe(p1, p2, current, winner, board);
+//		test.confirmation();
+		
+		PlayOptions test = new PlayOptions();
+		test.playOptions();
+	}
+}
